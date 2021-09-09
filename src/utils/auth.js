8d75e3843a4dcbca9de70830cb622b51b1decec7
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2021-09-09 19:48:36 +0200

    #8 add @adobe/aio-lib-core-logging lib

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -12,6 +12,8 @@ governing permissions and limitations under the License.
 const fs = require('fs')
 const path = require('path')
 const exchange = require('@adobe/aemcs-api-client-lib')
+const loggerNamespace = 'aem-headless-client-nodejs'
+const logger = require('@adobe/aio-lib-core-logging')(loggerNamespace, { level: process.env.LOG_LEVEL })
 const { SDKError } = require('./errors')
 
 /**
@@ -26,16 +28,20 @@ async function getToken (credentialsFilePath) {
   try {
     const filePath = path.isAbsolute(credentialsFilePath) ? credentialsFilePath : path.join(process.cwd(), credentialsFilePath)
     authFileContent = fs.readFileSync(filePath, 'utf8')
+    logger.debug('auth file read successfully')
   } catch (error) {
     const { name, message, details } = error
+    logger.debug('auth file read error', error)
     throw new SDKError(name, 'readFileSync', '', message, details)
   }
 
   let config = null
   try {
     config = JSON.parse(authFileContent)
+    logger.debug('auth file parsed successfully')
   } catch (error) {
     const { name, message, details } = error
+    logger.debug('auth file parse error', error)
     throw new SDKError(name, 'JSON.parse', '', message, details)
   }
 
@@ -50,6 +56,7 @@ async function getToken (credentialsFilePath) {
 
   return exchange(config)
     .then(data => {
+      logger.debug('exchange token success')
       return {
         accessToken: data.access_token,
         type: data.token_type,
@@ -58,6 +65,7 @@ async function getToken (credentialsFilePath) {
     })
     .catch(error => {
       const { name, type, message, details } = error
+      logger.debug('exchange token error', error)
       throw new SDKError(name, type || 'Exchange Token', '', message, details)
     })
 }
